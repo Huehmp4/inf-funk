55323552baf3477a4feab6d2b20f1c1adf6a71ac
Huehmp4 <163345321+Huehmp4@users.noreply.github.com>
2025-11-19 23:06:39 +0100

    Update bryly.py

--- bryly.py
+++ bryly.py
@@ -6,11 +6,3 @@ def pros(a:float,b:float,c:float) -> float:
     return 2 * (a*b + b*c + a*c)
 def granbokrownpodkw(a,b) -> float:
     return (2*(2*(a**2) + plaskie.kwipro(a,b)))
-def ostpodkwa(a,h) -> float:
-    h = a
-    P_p = a**2
-    h_t = math.sqrt(h**2 + (a/2)**2)
-    P_t = (a * h_t)/2
-    P_b = 4 * P_t
-    P_c = P_p + P_b
-    return P_c
